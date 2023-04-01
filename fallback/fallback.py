43edcc701596b46cb9d931c66e9f4e083f8cfa31
Joyce <fancycreative246@gmail.com>
2023-03-31 23:06:03 -0400

    add fallback service

---
+++ fallback/fallback.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+
+@app.route('/get-result')
+def get_result():
+    try:
+        args = request.args
+                
+        petal_length= args.get('petal_length', default='0')
+        
+        if int(petal_length) < 2.5:
+            return Response(response='iris-setosa', status=200, mimetype='application/json')
+        else:
+            return Response(response='iris-versicolor', status=200, mimetype='application/json')
+    except:
+        return Response(response="", status=201, mimetype='application/json')
+s
+
+if __name__ == '__main__':
+    app.run("0.0.0.0")
